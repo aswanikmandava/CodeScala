751b7debcb4c1e6cfff18dc0c7afa05b039665c2
Aswani K Mandava <aswani.k.mandava@gmail.com>
2025-11-03 18:57:57 -0500

    Removed semicolons as those are optional. Replaced var with val

--- src/main/scala/ControlStructures.scala
+++ src/main/scala/ControlStructures.scala
@@ -1,24 +1,24 @@
 object ControlStructures {
   def main(args: Array[String]): Unit = {
-    var counter: Int = 0;
+    var counter: Int = 0
     while (counter < 5) {
-//      println(s"Counter: $counter");
-      println("Counter: " + counter);
-      counter += 1;
+//      println(s"Counter: $counter")
+      println("Counter: " + counter)
+      counter += 1
     }
 
-    var i = 0;
+    var i = 0
     do {
       // this block executes at least once
-      println("i = " + i);
-      i += 1;
-    } while (i < 3);
+      println("i = " + i)
+      i += 1
+    } while (i < 3)
 
     // for loop syntax
     // for (var <- RANGE)
     // for (var <- start to end)
     for (j <- 1 to 3){
-      println("j = " + j);
+      println("j = " + j)
     }
 
     // nested for loop
@@ -29,12 +29,12 @@ object ControlStructures {
     // loop through a list
     val fruits: List[String] = List("Mango", "Kiwi", "Grapes")
     for (item <- fruits) {
-      println("Fruit: " + item);
+      println("Fruit: " + item)
     }
 
     // loop through a list conditionally
     for (item <- fruits; if item.length > 4) {
-      println("Filtered Fruit: " + item);
+      println("Filtered Fruit: " + item)
     }
 
     // for loop as an expression
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,9 @@
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
-    var fname: String = "Aswani";
-    var lname: String = "Mandava"
-    var age: Int = 30;
+    val fname: String = "Aswani"
+    val lname: String = "Mandava"
+    val age: Int = 30
     // println(s"...") utilizes string interpolation, a powerful feature for
     // embedding variables and expressions directly within a string literal
     // variable substitution
@@ -15,7 +15,7 @@ object Main {
 
 object IfElseIfExample {
   def main(args: Array[String]): Unit = {
-    val playerName: String = "Sachin";
+    val playerName: String = "Sachin"
     if (playerName == "Sachin Tendulkar") {
       println("Indian")
     }
